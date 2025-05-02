72e09141504fdf026b71ba8bbc81af69b4453269
Avaneesh Sai Sammeta <23b0973@iitb.ac.in>
2025-05-03 00:58:48 +0530

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,31 @@
+
+import streamlit as st
+import model
+
+
+st.title("Recursive Deep Models for Semantic Compositionality Over a Sentiment Treebank")
+
+st.header("Training our model using SVM")
+
+acc = sst_5_modified.print_accuracy()
+st.text("The training accuracy is:" )
+st.text(acc)
+
+# name = st.text_input("Enter ur review about bahubali", "Type Here ...")
+
+# display the name when the submit button is clicked
+# .title() is used to get the input text string
+# if(st.button('Submit')):
+#     out = sst_2_logistic_regression.predict_sentiment(name)
+#     st.text(out)
+
+name1 = st.text_input("Enter ur review about bahubali, we will predict using sst5 trained model", "")
+
+# # display the name when the submit button is clicked
+# # .title() is used to get the input text string
+if(st.button('PREDICT')):
+    out = sst_5_modified.predict_sentiment_sentence(name1)
+    st.text(out)
+if(st.button('PREDICT1')):
+    out1 = sst_5_modified.predict_sentiment(name1)
+    st.text(out1)
