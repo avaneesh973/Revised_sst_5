a94a431fe284c26f911e7b79ffe34158ac6e053f
Avaneesh Sai Sammeta <23b0973@iitb.ac.in>
2025-05-03 14:48:16 +0530

    Update model.py

--- model.py
+++ model.py
@@ -41,8 +41,8 @@ test_labels = test_dataset['label']
 dev_sentences = dev_dataset['text']
 dev_labels = dev_dataset['label']
 
-sentences = train_sentences + test_sentences
-labels = train_labels + test_labels
+sentences = train_sentences
+labels = train_labels
 
 # Convert text sentences to number form using tfidf vectorizer
 vectorizer = TfidfVectorizer(
